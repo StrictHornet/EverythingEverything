3a75b6d2c84e13287639bbd56d6064cee2a1f413
StrictHornet <42406510+StrictHornet@users.noreply.github.com>
2020-11-22 22:08:18 +0100

    crash course

--- Py Crash Course/crash.py
+++ Py Crash Course/crash.py
@@ -1,3 +1,17 @@
-print("Hello World!")
-print("*" * 10)
-i = 9
+class Institution:
+ def __init__(self, name, field):
+  self.name = name
+  self.field = field
+  
+ def about(self):
+  print(f'{self.name} is a {self.field} intstitution.')
+  
+class Company(Institution):
+ pass
+ 
+ 
+nameOfComp = input("What is your company's name:")
+fieldOfComp = input("What is your company's field:")
+
+company = Company(nameOfComp, fieldOfComp)
+company.about()
