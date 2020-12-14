98b7f199a1e7d9a8ae7f2f228862edd7a06e1d3a
StrictHornet <42406510+StrictHornet@users.noreply.github.com>
2020-12-14 20:42:15 +0100

    everythingeverything

---
+++ hackerrank/Day of The Programmer.py
@@ -0,0 +1,9 @@
+uInput = input("What year is it? :- ")
+no_of_days_year = 232
+no_of_days_leapyear = 233
+no_of_days_1918 = 219
+
+if uInput < 1918:
+    if uInput % 4 == 0:
+        day = 265 - 232
+        
