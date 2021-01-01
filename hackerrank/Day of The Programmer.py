a462191ca8473af237c8a2602c4cadb74ceb6a52
StrictHornet <42406510+StrictHornet@users.noreply.github.com>
2021-01-01 20:24:15 +0100

    Day of Programmer Task

--- hackerrank/Day of The Programmer.py
+++ hackerrank/Day of The Programmer.py
@@ -1,9 +1,11 @@
-uInput = input("What year is it? :- ")
-no_of_days_year = 232
-no_of_days_leapyear = 233
-no_of_days_1918 = 219
-
-if uInput < 1918:
-    if uInput % 4 == 0:
-        day = 265 - 232
+def DOP():    
+    uInput = int(input("What year is it? :- "))
+    print(uInput)
+    if uInput == 1918:
+        print(f"26.09.{uInput}")
+    elif (uInput < 1918 and uInput % 4 == 0) or ((uInput > 1918) and (uInput % 400 == 0 or (uInput % 4 == 0 and uInput % 100 != 0))):
+        print(f"12.09.{uInput}")
+    else:
+        print(f"13.09.{uInput}")
         
+DOP()
