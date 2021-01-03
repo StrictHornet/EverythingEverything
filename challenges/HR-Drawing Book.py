1511a449271bb0328e1314403609444a1c1ff02e
StrictHornet <42406510+StrictHornet@users.noreply.github.com>
2021-01-03 10:41:19 +0100

    Completed Challenge.60

---
+++ challenges/HR-Drawing Book.py
@@ -0,0 +1,27 @@
+print("Enter number of pages and page number to get to.")
+n = int(input("Enter number of pages: "))
+p = int(input("Enter page number: "))
+front = 1
+back = n
+fcount = 0
+bcount = 0
+minFlips = 0
+
+while(front<p):
+    front += 2
+    fcount += 1
+
+if n%2==0:
+    while(back>p):
+        back -= 2
+        bcount += 1
+else:
+    while(back>(p+1)):
+        back -= 2
+        bcount += 1
+if fcount < bcount:
+    minFlips = fcount
+else:
+    minFlips = bcount
+
+print(minFlips)
