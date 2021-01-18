f13702f998dc43b122c2e3d6669a823ca54b65a7
StrictHornet <42406510+StrictHornet@users.noreply.github.com>
2021-01-18 14:04:16 +0100

    HR-Picking Numbers.60

--- challenges/HR-Picking Numbers.py
+++ challenges/HR-Picking Numbers.py
@@ -1,7 +1,29 @@
-#[2,3,1,2,2,4,5,5,4]
+a = [2,3,1,1,2,2,4,5,5,4]
 
-variable = a[0]
 array = []
+b = sorted(a)
+print(b)
+index = 0
+i = 1
+setCount = 0
 
-for element in a:
-    
+for element in b:
+    count = 1
+    index += 1
+    if element in array:
+        continue
+    else:
+        array.append(element)
+    i = index
+    while(i < len(b)):
+        if b[i] - element < 2:
+            count += 1
+        else:
+            break
+        i += 1
+    print(count)    
+    if count > setCount:
+        setCount = count
+
+print(setCount)
+print(array)
