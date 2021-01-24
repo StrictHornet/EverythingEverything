d69efbcfb44f5424104ad77513414a540ce6af9d
StrictHornet <42406510+StrictHornet@users.noreply.github.com>
2021-01-24 15:09:53 +0100

    LC-strStr.90

---
+++ challenges/LC-strStr.py
@@ -0,0 +1,33 @@
+def strStr():
+    haystack = "mississippi"
+
+    needle = "issipi"
+    i = 0
+    j = 0
+
+    if needle == "":
+        return 0
+    if len(needle) > len(haystack):
+            return -1
+
+    for letter in haystack:
+        if needle[j] == letter and len(needle) <= (len(haystack)-i):
+            index = i
+            while(j<len(needle)-1):
+                wrongString = 0
+                j += 1
+                i += 1
+                if needle[j] == haystack[i]:
+                    continue
+                else:
+                    print(i, j)
+                    i = index
+                    j = 0
+                    wrongString = 1
+                    break
+            if wrongString != 1:
+                return index
+        i += 1
+    return -1
+
+print(strStr())
