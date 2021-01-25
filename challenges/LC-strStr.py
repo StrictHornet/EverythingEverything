ededd5a22f5187c0fb23765964478ebe9dfefbb6
StrictHornet <42406510+StrictHornet@users.noreply.github.com>
2021-01-25 09:49:14 +0100

    Optimisation Sol

--- challenges/LC-strStr.py
+++ challenges/LC-strStr.py
@@ -2,6 +2,11 @@ def strStr():
     haystack = "mississippi"
 
     needle = "issipi"
+
+    # for i in range(len(haystack) - len(needle)+1):
+    #     if haystack[i:i+len(needle)] == needle:
+    #         return i
+    # return -1
     i = 0
     j = 0
 
