acd40d1cd00c1cdc47cda48301be60dcb6fa35af
StrictHornet <42406510+StrictHornet@users.noreply.github.com>
2021-01-26 20:58:08 +0100

    Optimisation

--- challenges/HR-Counting Valleys.py
+++ challenges/HR-Counting Valleys.py
@@ -2,10 +2,7 @@ path = "UDDDUDUUDDUU"
 start = 0
 count = 0
 for element in path:
-    if element == "U":
-        start += 1
-    else:
-        start += -1
+    start += 1 if element == "U" else -1
     if element == "U" and start == 0:
         count += 1
 print(count)
