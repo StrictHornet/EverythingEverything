d4aad5d73a50f17fa0ff9999a661badfd2722934
StrictHornet <42406510+StrictHornet@users.noreply.github.com>
2021-02-01 17:40:48 +0100

    learning

--- Jotter/Loop Debug.py
+++ Jotter/Loop Debug.py
@@ -1,6 +1,17 @@
 i=0
 
-while(i<10):
-    for e in range(5):
-        print(i)
-        i+=1
+# while(i<10):
+#     for e in range(5):
+#         print(i)
+#         i+=1
+
+for alp in "Alphabet":
+    x = i * 2.4
+    if x>1 and x%2 == 0:
+        continue
+    print(f"Altitude is {alp}{x}")
+    i+=1
+
+
+for no in range(0, 101, 5):
+    print(no)
