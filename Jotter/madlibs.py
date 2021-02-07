9475387294c0d7dca4b8a8f923c1fa8f8a85eff1
StrictHornet <42406510+StrictHornet@users.noreply.github.com>
2021-02-07 19:28:15 +0100

    Simple Madlibs Implementation

---
+++ Jotter/madlibs.py
@@ -0,0 +1,6 @@
+adj = input("Input an adjective: ")
+verb1 = input("Input a verb: ")
+verb2 = input("Input a verb: ")
+
+madlib = f"I am a boy who is {adj}. I love to {verb1} and {verb2}"
+print(madlib)
