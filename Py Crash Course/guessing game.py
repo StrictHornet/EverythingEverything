5b2590dcc294005b2d1ee70beacabd85c95bbd3a
StrictHornet <42406510+StrictHornet@users.noreply.github.com>
2021-02-08 07:10:45 +0100

    No comment

---
+++ Py Crash Course/guessing game.py
@@ -0,0 +1,19 @@
+import random
+
+def guess(x, count):
+    random_num = random.randint(1, x)
+    while(count > 0):
+        user_guess = int(input("What is your guess: "))
+        if user_guess == random_num:
+            print("You guessed right!!!")
+            break
+        elif(user_guess < random_num):
+            print("You guessed a lesser number!")
+        else:
+            print("You guessed a higher number!")
+        count -= 1
+
+x = int(input("You want the computer to guess from 1 to: "))
+count = int(input("How many tries do you want? : "))
+guess(x, count)
+print("GAME OVER!")
