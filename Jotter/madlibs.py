9680998e8d04009738971f7350c9781f6fb371f0
StrictHornet <42406510+StrictHornet@users.noreply.github.com>
2021-02-08 06:40:54 +0100

    Update

--- Jotter/madlibs.py
+++ Jotter/madlibs.py
@@ -1,6 +1,8 @@
 adj = input("Input an adjective: ")
 verb1 = input("Input a verb: ")
 verb2 = input("Input a verb: ")
+famous_person = input("Input a famous person: ")
 
-madlib = f"I am a boy who is {adj}. I love to {verb1} and {verb2}"
+madlib = f"I am a boy who is {adj}. I love to {verb1} and {verb2}. Maybe one day I'll \
+be fly like {famous_person}"
 print(madlib)
