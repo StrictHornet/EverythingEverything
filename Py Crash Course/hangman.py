a07aed19b71ad1e2e058bbf6af16472c5e9f1903
StrictHornet <42406510+StrictHornet@users.noreply.github.com>
2021-02-12 12:14:31 +0100

    Hangman Completion

--- Py Crash Course/hangman.py
+++ Py Crash Course/hangman.py
@@ -10,22 +10,39 @@ def word_choice():
     return word
 
 def play_hangman():
-    word = word_choice()
-    lives = 10
+    word = word_choice().upper()
+    lives = 5
     used_letters = set()
     word_letters = set(word)
     print(word_letters)
     alphabet = set(string.ascii_uppercase)
-    while lives != 0:
+    while lives != 0 and bool(word_letters) is True:
+        word_list = [] #word_list = [letter if letter in used_letters else "-" for letter in word]
+        for letter in word:
+            if letter in used_letters:
+                word_list.append(letter)
+            else:
+                word_list.append("-")
+        print("The word to guess is", " ".join(word_list))
+        print(f"You have {lives} lives left. You have guessed the words {used_letters}")
         u_input = input("Guess a letter to complete the word: ").upper()
         if u_input in alphabet - used_letters:
             used_letters.add(u_input)
             if u_input in word_letters:
-                print("You guessed a word correctly :)")
+                print("You guessed a word correctly :)\n")
+                word_letters.remove(u_input)
             else:
-                print("You guessed a word incorrectly :(")
+                print("You guessed a word incorrectly :(\n")
                 lives -= 1
+        elif u_input in used_letters:
+            print(f"You've used letter {u_input} \n")
         else:
-            print(f"You've used letter {u_input}")
+            print("Invalid character. Try again.")
+
+    if lives == 0:
+        print("Your lives have finished. \nYou are Dead!")
+        print(f"The word was {word}")
+    else:
+        print(f"##### You guessed the word {word} correctly! #####")
 
 play_hangman()
