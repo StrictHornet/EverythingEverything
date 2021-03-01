ee603982c366f9537b9d7a314ddfba1c9dc34142
StrictHornet <42406510+StrictHornet@users.noreply.github.com>
2021-03-01 11:05:25 +0100

    JS

--- Jotter/JS Basics/index.js
+++ Jotter/JS Basics/index.js
@@ -1,8 +1,18 @@
-let fname = "Ehimare";
-console.log("Welcome " + fname);
+words = ["aback","abaft","abandoned","abashed","aberrant","abhorrent","abiding","abject","ablaze","able","abnormal","aboard","aboriginal","abortive","abounding","abrasive","abrupt","absent","absorbed","absorbing","abstracted","absurd","abundant","abusive","acceptable","accessible","accidental","accurate","acid","acidic","acoustic","acrid","actually","ad hoc","adamant","adaptable"]
 
-function JohnSmith(name, lname){
-    console.log("John Smith")
+rand_int = Math.floor(Math.random() * 30)
+document.getElementById("rand_int").innerHTML = rand_int
+rand_word = words[rand_int]
+var letter;
+var guessed_letters = ["a", "b"]
+for (letter in rand_word){
+    console.log(rand_word[letter])
+    if (guessed_letters.includes(rand_word[letter]))
+    {
+        document.getElementById("rand_word").innerHTML += rand_word[letter]
+    }
+    else
+    { 
+        document.getElementById("rand_word").innerHTML += "*"
+    }
 }
-
-JohnSmith();
