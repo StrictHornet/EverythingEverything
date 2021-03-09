118f6c61d7f4b26d77ba6b3a0cdca5b88b17efdd
StrictHornet <42406510+StrictHornet@users.noreply.github.com>
2021-03-09 11:19:42 +0100

    Tracker

---
+++ Jotter/Trascker.js
@@ -0,0 +1,15 @@
+//REVERSE SRTING FUNCTION (10 DAYS OF JS)
+function reverseString(s) {
+    try{
+        var split_s = s.split("");
+        var concat_s = "";
+        for(let x of split_s){
+            concat_s = x.concat(concat_s);
+    }
+        s = concat_s;
+    }
+    catch(e){
+        console.log(e.message);
+    }
+    console.log(s);
+}
