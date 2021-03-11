551958a323ae734baa4aeccb7cb7e66f96cc6e7b
StrictHornet <42406510+StrictHornet@users.noreply.github.com>
2021-03-11 12:18:36 +0100

    11-03-2021

--- Jotter/Trascker.js
+++ Jotter/Trascker.js
@@ -16,3 +16,21 @@ function reverseString(s) {
 
 //10-03-2021
 //Multiple Linear regressor on google colabs
+
+/*
+ * Complete the isPositive function.
+ * If 'a' is positive, return "YES".
+ * If 'a' is 0, throw an Error with the message "Zero Error"
+ * If 'a' is negative, throw an Error with the message "Negative Error"
+ */
+function isPositive(a) {
+    if(a>0){
+        return "YES"
+    }
+    else if(a == 0){
+        throw new Error("Zero Error");
+    }
+    else{
+        throw new Error("Negative Error")
+    }
+}
