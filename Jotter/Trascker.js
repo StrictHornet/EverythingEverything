23b33352639a042ae953a238d1b5d195c03b59b4
StrictHornet <42406510+StrictHornet@users.noreply.github.com>
2021-03-16 12:20:17 +0100

    Tracker

--- Jotter/Trascker.js
+++ Jotter/Trascker.js
@@ -49,3 +49,19 @@ function Rectangle(a, b) {
     
     return Quad
 }
+
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ * 
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+    var count = 0;
+    for(let object of objects){
+        if(object.x == object.y){
+            count++;
+        }
+    }
+    return count;
+}
