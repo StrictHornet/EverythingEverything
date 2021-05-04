3b89e4a7d344aadc84ca8080c2991966a0da3e58
StrictHornet <42406510+StrictHornet@users.noreply.github.com>
2021-05-04 15:39:11 +0100

    CS50

--- Jotter/Trascker.js
+++ Jotter/Trascker.js
@@ -332,7 +332,15 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // #print(str_types)
 //check50 cs50/problems/2021/x/dna
 
-
-
-
-
+// LAB 7 SQL
+// SELECT name FROM songs WHERE name LIKE "%feat%";
+// SELECT AVG(energy) FROM songs WHERE artist_id IN(SELECT id FROM artists WHERE name = "Drake");
+// SELECT name FROM songs WHERE artist_id IN(SELECT id FROM artists WHERE name = "Post Malone");
+// SELECT AVG(energy) FROM songs;
+// SELECT name FROM songs WHERE danceability > 0.75 AND energy > 0.75 AND valence > 0.75;
+// SELECT name FROM songs
+// ORDER BY duration_ms
+// DESC LIMIT 5;
+// SELECT name FROM songs
+// ORDER BY tempo;
+// SELECT name FROM songs;
