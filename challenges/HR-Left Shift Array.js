d44d16cefebcb60b86a4667d773f363c8509207d
StrictHornet <okosunprincewill@gmail.com>
2021-10-13 07:33:55 +0100

    HR-Left Shift Array Attempt 2

--- challenges/HR-Left Shift Array.js
+++ challenges/HR-Left Shift Array.js
@@ -19,3 +19,28 @@ function rotLeft(a, d) {
     
     return array1;
 }
+
+/*
+VERSION 2 USING RECURSION
+*/
+function rotLeft(a, d) {
+    // Write your code here
+    var rotations = d;
+    var A1 = a;
+    var A2 = [];
+    var left = A1[0];
+    var i = 1;
+    A1.shift();
+    A2 = A1;
+    A2.push(left);
+    rotations -= 1;
+    if(rotations > 0){
+        A1 = rotLeft(A2, rotations);
+    }
+    else{
+        //console.log(A2);
+        return A2;
+    }
+    console.log(A1);
+    return A1;
+}
