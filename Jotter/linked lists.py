b60b16d07ca23221ac9522ddab52cd9d0440bd53
StrictHornet <okosunprincewill@gmail.com>
2021-12-10 09:15:40 +0000

    Grind

--- Jotter/linked lists.py
+++ Jotter/linked lists.py
@@ -18,3 +18,17 @@ node1.next = node2
 node2.next = node3
 
 print((node1.next).data)
+
+while list1 and list2:
+    if list1.val <= list2.val:
+        tail.next = list1
+        list1 = list1.next
+    else:
+        tail.next = list2
+        list2 = list2.next
+    tail = tail.next
+if list1:
+    tail.next = list1
+elif list2:
+    tail.next = list2
+return dummy.next
