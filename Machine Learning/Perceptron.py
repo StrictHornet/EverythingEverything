da91d189e8a5e29397c5000def67ccc176bb226e
StrictHornet <okosunprincewill@gmail.com>
2022-02-24 23:35:25 +0000

    Perceptron Update

--- Machine Learning/Perceptron.py
+++ Machine Learning/Perceptron.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib as pt #for . pyplot
-from sklearn.impute import SimpleImputer
+#from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 
 
@@ -19,7 +19,10 @@ class Perceptron:
       self.weights = np.zeros(n_features)
       self.bias = 0
 
-      y_ = np.array([4 if i >= 3 else 2 for i in y])# == "class-1"
+      j = y[1]
+      print(j, "is represented as the binary digit 1, secondary comparison class is represented as 0")
+      y_ = np.array([1 if i == j else 0 for i in y])# == "class-1"
+      #print(y_)
 
       for _ in range(self.iters): #Number of iterations
         for index, Xsample in enumerate(X):
@@ -37,7 +40,7 @@ class Perceptron:
       return y
 
     def stepwise(self, x):
-      return(np.where(x >= 0, 4, 2))
+      return(np.where(x >= 0, 1, 0))
       #return 1 if x >= 0 else 0
 
 
@@ -47,15 +50,20 @@ class Perceptron:
 #Predict
 
 def accuracy(y_true, y_pred):
+  #accuracy = np.sum(y_true == y_pred) / len(y_true)
   accuracy = np.sum(y_true == y_pred) / len(y_true)
   return accuracy
 
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('traindata.csv')
+dataset.columns = ["A", "B", "C", "D", "Class"]
+
+#########################################
+"""
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, shuffle=False)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
 
-brain = Perceptron(learning_rate = 0.1, iters = 1000)
+brain = Perceptron(learning_rate = 0.1, iters = 20)
 brain.fit(X_train, y_train)
 predictions = brain.predict(X_test)
 
@@ -63,4 +71,29 @@ a = X_test[:, 0:1]
 print(a.transpose())
 print("Classification predictions", predictions)
 print(accuracy(y_test, predictions))
+"""
+##########################################
 
+#CLASSIFICATION
+#Drop all class 1 objects
+dataset = dataset[dataset["Class"].str.contains("class-1") == False]
+X = dataset.iloc[:, 0:-1].values
+y = dataset.iloc[:, -1].values
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
+#print(X_train)
+#print(y_train)
+
+brain = Perceptron(learning_rate = 0.9, iters = 80)
+brain.fit(X, y)
+
+testset = pd.read_csv('testdat.csv')
+testset.columns = ["A", "B", "C", "D", "Class"]
+testset = testset[testset["Class"].str.contains("class-1") == False]
+X_test = testset.iloc[:, 0:-1].values
+y_test = testset.iloc[:, -1].values
+y_test = np.array([1 if i == "class-2" else 0 for i in y_test])
+predictions = brain.predict(X_test)
+print(X_test)
+print(predictions)
+print(accuracy(y_test, predictions))
+print(brain.predict([597, 2.8, 5.1, 1.3]))
