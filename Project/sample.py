089a3c03ee420a4227590db15bb91e1d88b0eb41
StrictHornet <okosunprincewill@gmail.com>
2022-08-18 23:58:35 +0100

    Regression

--- Project/sample.py
+++ Project/sample.py
@@ -92,5 +92,28 @@ print(old_data_label)
 
 existing_label = np.concatenate([csvdata, old_data_label], axis = 1)
 print(existing_label)
+###
+#Log Reg Model
+###
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
+
+train_bow = old_data
+# test_bow = bow[31962:,:]
+
+# splitting data into training and validation set
+xtrain_bow, xvalid_bow, ytrain, yvalid = train_test_split(train_bow, train['label'], random_state=42, test_size=0.3)
+
+lreg = LogisticRegression()
+lreg.fit(xtrain_bow, ytrain) # training the model
+
+prediction = lreg.predict_proba(xvalid_bow) # predicting on the validation set
+prediction_int = prediction[:,0] >= 0.3 # if prediction is greater than or equal to 0.3 than 1 else 0
+prediction_int = prediction_int.astype(np.int)
+
+good_job = f1_score(yvalid, prediction_int) # calculating f1 score
+print(good_job)
 
 #Stopped at converting glove vectors to numpy array to allow it be formatted as input data for logistic regression model
