16acfabcf2935e812c11b2823b80c70bf6305ddb
StrictHornet <42406510+StrictHornet@users.noreply.github.com>
2022-11-09 23:55:48 +0000

    Update leap_year.py

--- challenges/leap_year.py
+++ challenges/leap_year.py
@@ -1,7 +1,8 @@
+#Checks if a year is a leap year or not
+
 def is_leap(year):
     leap = False
     
-    # Write your logic here
     if year % 4 == 0:
         leap = True
         if year % 100 == 0:
